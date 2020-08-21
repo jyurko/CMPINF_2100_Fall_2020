f0d9c7f85f0b4dad42e5cb1caf42de7ecc9c2191
Joseph P. Yurko <jyurko@gmail.com>
2020-08-20 22:13:14 -0400

    week 01 variables, types, list

---
+++ week_01/in_class_script_8_20_2020.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 20 20:44:07 2020
+
+@author: XPS15
+"""
+
+2 + 2
+
+3 + 3
+
+7 + 2
+
+# %%
+
+print( 3 + 7 )
+
+print( 8 + 11 )
+
+print( type( 1.11 ) )
+
+
+# %%
+
+x = 3
+
+y = 7.1
+
+print( type(x) )
+
+print( type(y) )
+
+print( type(x + y) )
+
+# %%
+
+print( x ** 2 )
+
+print( y ** 3 )
+
+# %%
+
+print( x / y)
+
+
+# %%
+
+z = 5
+
+# x + z
+# a line of code not executed by the intepreter
+
+# %%
